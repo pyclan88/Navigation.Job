a8275feb8c9eefa883333f54b14538ecfd332531
pyclan88 <gumabemc@yandex.ru>
2024-11-09 20:14:08 +0300

    исправил порядок импортов

--- app/src/main/java/ru/practicum/android/diploma/ui/team/TeamFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/team/TeamFragment.kt
@@ -4,8 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import ru.practicum.android.diploma.databinding.FragmentTeamBinding
+
 import ru.practicum.android.diploma.R
+import ru.practicum.android.diploma.databinding.FragmentTeamBinding
 import ru.practicum.android.diploma.util.BindingFragment
 
 class TeamFragment : BindingFragment<FragmentTeamBinding>() {
