9043152dcf973bb037ce06bb1f640e267cee89e0
Nikita <trenin.0235@gmail.com>
2024-11-10 20:23:59 +0700

    task-23 Добавил @PrimaryKey в VacancyEntity

--- app/src/main/java/ru/practicum/android/diploma/data/db/entity/VacancyEntity.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/db/entity/VacancyEntity.kt
@@ -1,6 +1,10 @@
 package ru.practicum.android.diploma.data.db.entity
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity
-class VacancyEntity
+class VacancyEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long
+)
