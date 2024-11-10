b1e2b36fa2752354df4126e312667fbd4afb5bf3
Nikita <trenin.0235@gmail.com>
2024-11-10 20:07:55 +0700

    task-23 Убрал пусты строки

--- app/src/main/java/ru/practicum/android/diploma/data/dto/VacancySearchResponse.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/dto/VacancySearchResponse.kt
@@ -1,6 +1,5 @@
 package ru.practicum.android.diploma.data.dto
 
-
 import com.google.gson.annotations.SerializedName
 import ru.practicum.android.diploma.data.dto.vacancy.ItemDto
 
--- app/src/main/java/ru/practicum/android/diploma/data/dto/vacancy/ItemDto.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/dto/vacancy/ItemDto.kt
@@ -1,6 +1,5 @@
 package ru.practicum.android.diploma.data.dto.vacancy
 
-
 import com.google.gson.annotations.SerializedName
 
 data class ItemDto(
