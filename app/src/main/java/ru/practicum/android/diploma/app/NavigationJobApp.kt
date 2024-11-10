959e26b79fc202cb28e0134dedf3341fe0a80a3d
Marsel <marsel21vek@ya.ru>
2024-11-10 12:19:38 +0500

    Добавил BottomNavigationView и модули.

--- app/src/main/java/ru/practicum/android/diploma/app/NavigationJobApp.kt
+++ app/src/main/java/ru/practicum/android/diploma/app/NavigationJobApp.kt
@@ -2,8 +2,20 @@ package ru.practicum.android.diploma.app
 
 import android.app.Application
 import android.content.Context
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
+import ru.practicum.android.diploma.R
+import ru.practicum.android.diploma.databinding.ActivityRootBinding
+import ru.practicum.android.diploma.di.dataModule
+import ru.practicum.android.diploma.di.interactorModule
+import ru.practicum.android.diploma.di.repositoryModule
+import ru.practicum.android.diploma.di.utilModule
+import ru.practicum.android.diploma.di.viewModelModule
 
 class NavigationJobApp : Application() {
 
@@ -15,14 +27,12 @@ class NavigationJobApp : Application() {
         super.onCreate()
         startKoin {
             androidContext(this@NavigationJobApp)
-            modules()
+            modules(dataModule, interactorModule, repositoryModule, viewModelModule, utilModule)
         }
-
     }
 
     companion object {
         private var instance: NavigationJobApp? = null
-
         fun applicationContext(): Context {
             return instance!!.applicationContext
         }
--- app/src/main/java/ru/practicum/android/diploma/di/UtilModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/UtilModule.kt
@@ -3,5 +3,4 @@ package ru.practicum.android.diploma.di
 import org.koin.dsl.module
 
 val utilModule = module {
-
 }
