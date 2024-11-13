4b3f00cffc530c2de04e59da71be024152f3ab66
Alexander Kireev <sashkir7@mail.ru>
2024-11-14 00:12:29 +0300

    Search view model

--- app/src/main/java/ru/practicum/android/diploma/ui/search/VacanciesHolder.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/search/VacanciesHolder.kt
@@ -1,10 +1,26 @@
 package ru.practicum.android.diploma.ui.search
 
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.databinding.VacancyItemBinding
+import ru.practicum.android.diploma.domain.models.Vacancy
 
 class VacanciesHolder(
     private val binding: VacancyItemBinding
 ) : RecyclerView.ViewHolder(binding.root) {
-    // fun bind(model: Vacancy){}
+
+    fun bind(vacancy: Vacancy) = with(binding) {
+        vacancy.imageUrl?.let { setLogo(it) }
+
+        vacancyTitle.text = vacancy.name
+        vacancyEmployer.text = vacancy.employerName
+        vacancySalary.text = vacancy.salaryFrom
+    }
+
+    private fun setLogo(url: String) = Glide.with(itemView)
+        .load(url)
+        .centerCrop()
+        .placeholder(R.drawable.smile)
+        .into(binding.vacancyLogo)
 }
