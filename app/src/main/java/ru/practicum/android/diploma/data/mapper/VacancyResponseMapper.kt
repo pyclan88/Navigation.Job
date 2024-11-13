d93c6fa91cc1f51945176d0a1e8281da12394c94
Cain <decardcain21@gmail.com>
2024-11-13 16:06:41 +0300

    task-33: try to fix "The function map appears to be too complex based on Cyclomatic Complexity (complexity: 10). Defined complexity threshold for methods is set to '10'"

--- app/src/main/java/ru/practicum/android/diploma/data/mapper/VacancyResponseMapper.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/mapper/VacancyResponseMapper.kt
@@ -1,29 +1,35 @@
 package ru.practicum.android.diploma.data.mapper
 
 import ru.practicum.android.diploma.data.dto.VacancySearchResponse
+import ru.practicum.android.diploma.data.dto.vacancy.ItemDto
 import ru.practicum.android.diploma.domain.models.Vacancy
 
 class VacancyResponseMapper {
     fun map(response: VacancySearchResponse): List<Vacancy> {
-        return response.items.map { vacancy ->
-            Vacancy(
-                id = vacancy.id,
-                imageUrl = vacancy.employer.url,
-                name = vacancy.name,
-                city = vacancy.area?.name ?: "Не указано", // Значение по умолчанию
-                salaryFrom = vacancy.salary?.from?.toString() ?: "Не указано", // Значение по умолчанию
-                salaryTo = vacancy.salary?.to?.toString() ?: "Не указано", // Значение по умолчанию
-                currency = vacancy.salary?.currency ?: "Не указана", // Значение по умолчанию
-                employerName = vacancy.employer.name,
-                experience = vacancy.experience?.name ?: "Не указано", // Значение по умолчанию
-                employmentName = vacancy.employment?.name ?: "Не указано", // Значение по умолчанию
-                schedule = vacancy.schedule?.name ?: "Не указано", // Значение по умолчанию
-                descriptionResponsibility = vacancy.snippet.responsibility ?: "Не указано", // Значение по умолчанию
-                descriptionRequirement = vacancy.snippet.requirement ?: "Не указано", // Значение по умолчанию
-                descriptionConditions = "???", // Может быть заполнено позже
-                descriptionSkills = "???" // Может быть заполнено позже
-            )
+        return response.items.map { vacancy -> createVacancy(vacancy) }
+    }
 
-        }
+    fun createVacancy(vacancy: ItemDto): Vacancy {
+        return Vacancy(
+            id = vacancy.id,
+            imageUrl = vacancy.employer.url,
+            name = vacancy.name,
+            city = getValueOrDefault(vacancy.area?.name, "Не указано"),
+            salaryFrom = getValueOrDefault(vacancy.salary?.from?.toString(), "Не указано"),
+            salaryTo = getValueOrDefault(vacancy.salary?.to?.toString(), "Не указано"),
+            currency = getValueOrDefault(vacancy.salary?.currency, "Не указана"),
+            employerName = vacancy.employer.name,
+            experience = getValueOrDefault(vacancy.experience?.name, "Не указано"),
+            employmentName = getValueOrDefault(vacancy.employment?.name, "Не указано"),
+            schedule = getValueOrDefault(vacancy.schedule?.name, "Не указано"),
+            descriptionResponsibility = getValueOrDefault(vacancy.snippet.responsibility, "Не указано"),
+            descriptionRequirement = getValueOrDefault(vacancy.snippet.requirement, "Не указано"),
+            descriptionConditions = "???", // Может быть заполнено позже
+            descriptionSkills = "???" // Может быть заполнено позже
+        )
+    }
+
+    fun <T> getValueOrDefault(value: T?, defaultValue: String): String {
+        return value?.toString() ?: defaultValue
     }
 }
