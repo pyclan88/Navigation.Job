2142a85771580e7b9736991fa09ccd9a1bd92ae3
Nikita <trenin.0235@gmail.com>
2024-11-14 16:58:39 +0700

    task-39 Добавил обработку пустого состояние в SearchFragment

--- app/src/main/java/ru/practicum/android/diploma/ui/search/SearchFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/search/SearchFragment.kt
@@ -5,11 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.widget.doOnTextChanged
+import org.koin.android.ext.android.inject
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import ru.practicum.android.diploma.R
+import ru.practicum.android.diploma.common.ImageAndTextHelperImpl
 import ru.practicum.android.diploma.databinding.FragmentSearchBinding
 import ru.practicum.android.diploma.domain.state.VacancyState
 import ru.practicum.android.diploma.domain.state.VacancyState.VacanciesList.Data
 import ru.practicum.android.diploma.util.BindingFragment
+import ru.practicum.android.diploma.util.ImageAndTextHelper
 import ru.practicum.android.diploma.util.invisible
 import ru.practicum.android.diploma.util.visible
 
@@ -17,6 +21,7 @@ class SearchFragment : BindingFragment<FragmentSearchBinding>() {
 
     private val vacanciesAdapter = VacanciesAdapter()
     private val viewModel: SearchViewModel by viewModel()
+    private val imageAndTextHelper: ImageAndTextHelper by inject()
 
     override fun createBinding(
         inflater: LayoutInflater,
@@ -34,12 +39,27 @@ class SearchFragment : BindingFragment<FragmentSearchBinding>() {
 
     private fun render(state: VacancyState) {
         when (state.vacanciesList) {
-            is VacancyState.VacanciesList.Empty -> {}
+            is VacancyState.VacanciesList.Empty -> with(binding) {
+                rvVacancies.invisible()
+                pbSearch.invisible()
+                ivLookingForPlaceholder.invisible()
+                groupPlaceholder.visible()
+                imageAndTextHelper.setImageAndText(
+                    requireContext(),
+                    binding.ivPlaceholder,
+                    binding.tvPlaceholder,
+                    R.drawable.placeholder_no_vacancy_list_or_region_plate_cat,
+                    resources.getString(R.string.no_vacancy_list)
+                )
+                groupPlaceholder.visible()
+            }
+
             is VacancyState.VacanciesList.NoInternet -> {}
             is VacancyState.VacanciesList.Loading -> with(binding) {
                 pbSearch.visible()
                 rvVacancies.invisible()
                 ivLookingForPlaceholder.invisible()
+                groupPlaceholder.invisible()
             }
 
             is VacancyState.VacanciesList.Error -> {}
@@ -49,6 +69,7 @@ class SearchFragment : BindingFragment<FragmentSearchBinding>() {
                 pbSearch.invisible()
                 ivLookingForPlaceholder.invisible()
                 vacanciesAdapter.setVacancies(state.vacanciesList.vacancies)
+                groupPlaceholder.invisible()
             }
         }
     }
