a7b7be056a324864dc427feb32c9c0bec314f06e
Nikita <trenin.0235@gmail.com>
2024-11-16 22:53:46 +0700

    task-49

--- app/src/main/java/ru/practicum/android/diploma/data/mapper/VacancyDetailsMapper.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/mapper/VacancyDetailsMapper.kt
@@ -4,7 +4,7 @@ import ru.practicum.android.diploma.data.dto.vacancy.details.VacancyDetailsDto
 import ru.practicum.android.diploma.domain.models.VacancyDetails
 import ru.practicum.android.diploma.util.JsonParser
 
-class VacancyDetailsMapper() {
+class VacancyDetailsMapper {
     private val jsonParser = JsonParser()
     fun map(dto: VacancyDetailsDto) = VacancyDetails(
         id = dto.id,
