6399f24dc84acc85bb404f146d6246c3d2696450
Nikita <trenin.0235@gmail.com>
2024-11-17 17:35:59 +0700

    task-56 - изменил название файла

--- app/src/main/java/ru/practicum/android/diploma/ui/favorite/FavoriteViewModel.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/favorite/FavoriteViewModel.kt
@@ -5,29 +5,29 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
-import ru.practicum.android.diploma.domain.state.VacancyListState
+import ru.practicum.android.diploma.domain.state.FavoriteVacanciesState
 
 class FavoriteViewModel : ViewModel() {
 //  private val getVacancyFromDbUseCase: GetVacancyFromDbUseCase
 
-    private val _state = MutableLiveData<VacancyListState>()
-    val state: LiveData<VacancyListState> get() = _state
+    private val _state = MutableLiveData<FavoriteVacanciesState>()
+    val state: LiveData<FavoriteVacanciesState> get() = _state
 
     fun getVacancyList() {
         viewModelScope.launch {
 //            val result = getVacancyFromDbUseCase.execute()
-//            val vacancyList: VacancyListState =
+//            val favoriteVacanciesState: FavoriteVacanciesState =
 //                when (result.first) {
 //                    null -> {
 //                        if (result.second == NOT_FOUND_CODE.toString()) {
-//                            VacancyListState.Empty
+//                            FavoriteVacanciesState.Empty
 //                        } else {
-//                            VacancyListState.Error
+//                            FavoriteVacanciesState.Error
 //                        }
 //                    }
-//                    else -> VacancyDetailsList.Data(result.first!!)
+//                    else -> FavoriteVacanciesState.Data(result.first!!)
 //                }
-//            _state.postValue(favoriteState)
+//            _state.postValue(favoriteVacanciesState)
         }
     }
 }
