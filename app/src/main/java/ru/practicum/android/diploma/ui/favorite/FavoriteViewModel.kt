de4f384bb01dac96582a8b4008829338d2c771f3
Alexander Kireev <sashkir7@mail.ru>
2024-11-18 13:01:00 +0300

    Refactoring favorite view model layer

--- app/src/main/java/ru/practicum/android/diploma/ui/favorite/FavoriteViewModel.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/favorite/FavoriteViewModel.kt
@@ -6,28 +6,23 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
 import ru.practicum.android.diploma.domain.state.FavoriteVacanciesState
+import ru.practicum.android.diploma.domain.usecase.favorite.GetAllFavoriteVacanciesUseCase
 
-class FavoriteViewModel : ViewModel() {
-//  private val getVacancyFromDbUseCase: GetVacancyFromDbUseCase
+class FavoriteViewModel(
+    private val getAllFavoriteVacanciesUseCase: GetAllFavoriteVacanciesUseCase
+) : ViewModel() {
 
     private val _state = MutableLiveData<FavoriteVacanciesState>()
     val state: LiveData<FavoriteVacanciesState> get() = _state
 
-    fun getVacancyList() {
-        viewModelScope.launch {
-//            val result = getVacancyFromDbUseCase.execute()
-//            val favoriteVacanciesState: FavoriteVacanciesState =
-//                when (result.first) {
-//                    null -> {
-//                        if (result.second == NOT_FOUND_CODE.toString()) {
-//                            FavoriteVacanciesState.Empty
-//                        } else {
-//                            FavoriteVacanciesState.Error
-//                        }
-//                    }
-//                    else -> FavoriteVacanciesState.Data(result.first!!)
-//                }
-//            _state.postValue(favoriteVacanciesState)
+    fun getFavoriteVacancies() = viewModelScope.launch {
+        val vacancies = getAllFavoriteVacanciesUseCase.execute()
+        val state = if (vacancies.isEmpty()) {
+            FavoriteVacanciesState.Empty
+        } else {
+            FavoriteVacanciesState.Data(vacancies)
         }
+
+        _state.postValue(state)
     }
 }
