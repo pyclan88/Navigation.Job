affadf4c077deb369468b80ee8be6d8d6d3b6c9e
Alexander Kireev <sashkir7@mail.ru>
2024-11-18 15:38:39 +0300

    Fix linter

--- app/src/main/java/ru/practicum/android/diploma/ui/vacancy/VacancyViewModel.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/vacancy/VacancyViewModel.kt
@@ -7,7 +7,12 @@ import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
 import ru.practicum.android.diploma.data.network.RetrofitNetworkClient.Companion.NOT_FOUND_CODE
 import ru.practicum.android.diploma.domain.state.VacancyDetailsState
-import ru.practicum.android.diploma.domain.state.VacancyDetailsState.*
+import ru.practicum.android.diploma.domain.state.VacancyDetailsState.Data.Empty
+import ru.practicum.android.diploma.domain.state.VacancyDetailsState.Data.Error
+import ru.practicum.android.diploma.domain.state.VacancyDetailsState.Data.Loading
+import ru.practicum.android.diploma.domain.state.VacancyDetailsState.Data.Payload
+import ru.practicum.android.diploma.domain.state.VacancyDetailsState.Favorite.InFavorite
+import ru.practicum.android.diploma.domain.state.VacancyDetailsState.Favorite.NotInFavorite
 import ru.practicum.android.diploma.domain.usecase.GetVacancyDetailsUseCase
 import ru.practicum.android.diploma.domain.usecase.favorite.AddVacancyToFavoriteUseCase
 import ru.practicum.android.diploma.domain.usecase.favorite.DeleteVacancyFromFavoriteUseCase
@@ -24,32 +29,33 @@ class VacancyViewModel(
     val state: LiveData<VacancyDetailsState> get() = _state
 
     init {
-        _state.postValue(VacancyDetailsState(Data.Loading, Favorite.NotInFavorite))
+        _state.postValue(VacancyDetailsState(Loading, NotInFavorite))
     }
 
     fun getVacancyDetails(id: String) = viewModelScope.launch {
         val details = getVacancyDetailsUseCase.execute(id)
         val detailsState = when (details.first) {
-            null -> if (details.second == NOT_FOUND_CODE.toString()) Data.Empty else Data.Error
-            else -> Data.Payload(details.first!!)
+            null -> if (details.second == NOT_FOUND_CODE.toString()) Empty else Error
+            else -> Payload(details.first!!)
         }
 
         val favorite = getFavoriteVacancyByIdUseCase.execute(id)
-        val favoriteState = if (favorite == null) Favorite.NotInFavorite else Favorite.InFavorite
+        val favoriteState = if (favorite == null) NotInFavorite else InFavorite
 
         _state.postValue(VacancyDetailsState(detailsState, favoriteState))
     }
 
+    @Suppress("LabeledExpression")
     fun onFavoriteClicked(vacancyId: String) = viewModelScope.launch {
         when (val dataState = _state.value?.data) {
-            is Data.Payload -> {
+            is Payload -> {
                 val dbFavoriteVacancy = getFavoriteVacancyByIdUseCase.execute(vacancyId)
                 val favoriteState = if (dbFavoriteVacancy == null) {
                     addVacancyToFavoriteUseCase.execute(dataState.details)
-                    Favorite.InFavorite
+                    InFavorite
                 } else {
                     deleteVacancyFromFavoriteUseCase.execute(dataState.details)
-                    Favorite.NotInFavorite
+                    NotInFavorite
                 }
 
                 _state.postValue(VacancyDetailsState(dataState, favoriteState))
