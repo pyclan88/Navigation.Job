23d4f228f1e4611953867aae7aad3227af8bed73
Alexander Kireev <sashkir7@mail.ru>
2024-11-18 10:42:42 +0300

    Remove unused import

--- app/src/main/java/ru/practicum/android/diploma/domain/api/VacancyRepository.kt
+++ app/src/main/java/ru/practicum/android/diploma/domain/api/VacancyRepository.kt
@@ -1,6 +1,5 @@
 package ru.practicum.android.diploma.domain.api
 
-import ru.practicum.android.diploma.domain.models.Vacancy
 import ru.practicum.android.diploma.domain.models.VacancyDetails
 import ru.practicum.android.diploma.domain.models.VacancySearchResult
 import ru.practicum.android.diploma.util.Resource
