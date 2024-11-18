dd157604471e529b632920bd408c42bcbac55225
Alexander Kireev <sashkir7@mail.ru>
2024-11-18 11:06:36 +0300

    Handle favorite vacancies repository

--- app/src/main/java/ru/practicum/android/diploma/di/RepositoryModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/RepositoryModule.kt
@@ -1,11 +1,27 @@
 package ru.practicum.android.diploma.di
 
 import org.koin.dsl.module
+import ru.practicum.android.diploma.data.FavoriteVacanciesRepositoryImpl
 import ru.practicum.android.diploma.data.VacancyRepositoryImpl
+import ru.practicum.android.diploma.data.db.AppDatabase
+import ru.practicum.android.diploma.domain.api.FavoriteVacanciesRepository
 import ru.practicum.android.diploma.domain.api.VacancyRepository
 
 val repositoryModule = module {
+
     single<VacancyRepository> {
-        VacancyRepositoryImpl(get(), get(), get())
+        VacancyRepositoryImpl(
+            networkClient = get(),
+            vacancyMapper = get(),
+            vacancyDetailsMapper = get()
+        )
     }
+
+    single<FavoriteVacanciesRepository> {
+        FavoriteVacanciesRepositoryImpl(
+            favoriteDao = get<AppDatabase>().favoriteVacanciesDao(),
+            convertor = get()
+        )
+    }
+
 }
