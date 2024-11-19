c901d8c2112775527702fd422d6373a7acc60ce6
Nikita <trenin.0235@gmail.com>
2024-11-19 22:28:46 +0700

    bugfix-83 - исправил отступы

--- app/src/main/java/ru/practicum/android/diploma/di/DataModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/DataModule.kt
@@ -25,11 +25,10 @@ val dataModule = module {
 
     single {
         Room.databaseBuilder(
-                context = androidContext(),
-                klass = AppDatabase::class.java,
-                name = "database.db"
-            ).fallbackToDestructiveMigration()
-            .build()
+            context = androidContext(),
+            klass = AppDatabase::class.java,
+            name = "database.db"
+        ).fallbackToDestructiveMigration().build()
 
     }
 
