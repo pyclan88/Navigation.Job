794b6ee076f75b931be0d6621ed543119664c9e9
Alexander Kireev <sashkir7@mail.ru>
2024-11-21 10:24:55 +0300

    Fix dto remarks

--- app/src/main/java/ru/practicum/android/diploma/data/dto/vacancy/nested/KeySkillDto.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/dto/vacancy/nested/KeySkillDto.kt
@@ -3,5 +3,6 @@ package ru.practicum.android.diploma.data.dto.vacancy.nested
 import com.google.gson.annotations.SerializedName
 
 data class KeySkillDto(
-    @SerializedName("name") val name: String
+    @SerializedName("name")
+    val name: String
 )
