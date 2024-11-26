f9b8e445eadccb0b110d8c569760acd31bf987f1
Nikita <trenin.0235@gmail.com>
2024-11-26 15:39:00 +0700

    task-124 - убрал сириализацию

--- app/src/main/java/ru/practicum/android/diploma/data/dto/industry/IndustryItemDto.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/dto/industry/IndustryItemDto.kt
@@ -1,12 +1,7 @@
 package ru.practicum.android.diploma.data.dto.industry
 
-import com.google.gson.annotations.SerializedName
-
 data class IndustryItemDto(
-    @SerializedName("id")
     val id: String,
-    @SerializedName("industries")
     val industries: List<IndustryDto>,
-    @SerializedName("name")
     val name: String
 )
