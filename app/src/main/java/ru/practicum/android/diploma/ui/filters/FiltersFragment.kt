13cbab62cdfcb904886393e9210b403ecc95c120
pyclan88 <gumabemc@yandex.ru>
2024-12-01 21:03:15 +0300

    bugfix-186: - исправил цвет "Ожидаемая зарплата"

--- app/src/main/java/ru/practicum/android/diploma/ui/filters/FiltersFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/filters/FiltersFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
 import androidx.core.widget.doOnTextChanged
 import androidx.lifecycle.lifecycleScope
@@ -98,6 +99,8 @@ class FiltersFragment : BindingFragment<FragmentFilterBinding>() {
 
     private fun configureWithoutSalaryCheckbox() = with(binding) {
         cbWithoutSalaryButton.setOnCheckedChangeListener { _, isChecked ->
+            val hintColor = if (isChecked) R.color.black else R.color.blue
+            tlSalaryLayout.hintTextColor = ContextCompat.getColorStateList(requireContext(), hintColor)
             viewModel.setWithoutSalaryButton(isChecked)
         }
     }
